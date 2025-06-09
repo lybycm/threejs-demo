865903201b5d313ea1245149e6f6f6581f009410
liuyang <liuy@testor.com.cn>
2025-06-09 14:29:54 +0800

    feat: 75 贴花几何体

---
+++ decal-geometry/src/main.js
@@ -0,0 +1,37 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import mesh from "./mesh.js";
+
+const scene = new THREE.Scene();
+scene.add(mesh);
+
+const directionLight = new THREE.DirectionalLight(0xffffff, 2);
+directionLight.position.set(500, 400, 300);
+scene.add(directionLight);
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 1000);
+camera.position.set(500, 600, 400);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+renderer.setSize(width, height);
+
+function render() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+render();
+
+document.body.append(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
---
+++ decal-geometry/src/mesh.js
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+import { DecalGeometry } from "three/addons/geometries/DecalGeometry.js";
+
+const loader = new THREE.TextureLoader();
+const texture = loader.load("./xiaoxin.png");
+texture.colorSpace = THREE.SRGBColorSpace;
+
+const group = new THREE.Group();
+
+const geometry = new THREE.SphereGeometry(200);
+const material = new THREE.MeshPhongMaterial({
+  color: "orange",
+});
+const mesh = new THREE.Mesh(geometry, material);
+group.add(mesh);
+
+const position = new THREE.Vector3(0, 0, 200);
+const orientation = new THREE.Euler();
+const size = new THREE.Vector3(100, 100, 100);
+const geometry2 = new DecalGeometry(mesh, position, orientation, size);
+const material2 = new THREE.MeshPhongMaterial({
+  // color: "green",
+  map: texture,
+  transparent: true,
+  polygonOffset: true,
+  polygonOffsetFactor: -4,
+});
+const mesh2 = new THREE.Mesh(geometry2, material2);
+group.add(mesh2);
+
+export default group;
